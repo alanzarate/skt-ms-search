e013ad9160c9d38615d9881462a0ebe7f8bdbc20
alanzarate <alnzarate@gmail.com>
2023-05-07 18:05:13 -0400

    first commit

---
+++ src/main/kotlin/com/ucb/bo/sktmssearch/SktMsSearchApplication.kt
@@ -0,0 +1,11 @@
+package com.ucb.bo.sktmssearch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SktMsSearchApplication
+
+fun main(args: Array<String>) {
+	runApplication<SktMsSearchApplication>(*args)
+}
